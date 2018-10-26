7ca016363c4094f1ae72b9fdefa23be87c6bbded
AOIDUO <30717258+UniverseObserver@users.noreply.github.com>
2018-10-26 16:14:24 +0800

    咕咕咕

--- bot.py
+++ bot.py
@@ -8,7 +8,8 @@ dispatcher = updater.dispatcher
 
 def stalk(bot, update):
     incoming = update.message.text
-    ret = 'I do not understand what you are talking about. '
+    #ret = 'I do not understand what you are talking about. '
+    ret = '咕咕咕 '
     if (incoming.lower() == 'hello') or (incoming.lower() == 'hi'):
         ret = 'Hi. I am Harry.'
     update.message.reply_text(ret)
