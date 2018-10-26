999efe017eb49c310096de235e37bda54e845ca3
AOIDUO <30717258+UniverseObserver@users.noreply.github.com>
2018-10-26 15:38:10 +0800

    i am harry

--- bot.py
+++ bot.py
@@ -7,28 +7,11 @@ updater = Updater(token=token)
 dispatcher = updater.dispatcher
 
 def stalk(bot, update):
-    api = requests.get("https://api.github.com/users/" + update.message.text)
-    res = api.json()
-    # print(api, res)
-    profile = ""
-    pic = res["avatar_url"] + "&s=128"
-    for data in res:
-        url = data.endswith('url')
-        ids = data.endswith('id')
-        hireable = data.endswith('hireable')
-        admin = data.endswith('admin')
-        updated = data.endswith('updated_at')
-        if url or ids or hireable or admin or updated:
-            pass
-        else:
-            copy = data
-            copy_res = res[data]
-            if copy == "created_at":
-                copy = "Joined"
-                copy_res = copy_res.split('T')[0]
-            profile += str(copy.title().replace("_", " ")) + ": " + str(copy_res) + "\n"
-    profile += pic
-    update.message.reply_text('x')
+    incoming = update.message.text
+    ret = 'I do not understand what you are talking about. '
+    if (incoming.lower() == 'hello') or (incoming.lower() == 'hi'):
+        ret = 'Hi. I am Harry.'
+    update.message.reply_text(ret)
 
 dispatcher.add_handler(MessageHandler(Filters.text, stalk))
 
